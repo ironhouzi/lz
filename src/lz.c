8fd279e97bb049f4499d0904e7214a27d6084a65
Robin <robinds@student.matnat.uio.no>
2014-11-26 20:46:49 +0100

    Functionality in place -- Need refactoring

--- src/lz.c
+++ src/lz.c
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <limits.h>
 #include "lz.h"
+#include <ncurses.h>
+
+#define MAXBUF 21
 
 Entry* new_entry(const char* string)
 {
@@ -90,14 +93,15 @@ int first_match(int *list, int length)
     return -1;
 }
 
-int calculate_penalty(int j, int *matches, int *previous_matches, int length)
+int calculate_penalty(const int *matches, const int *previous_matches,
+                      const int len_s, const int len_t, const int penalty, int *j)
 {
+    // TODO: maintain skip count accross all calculations
     int skip = 1;
     int found = 0;
-    int penalty = 0;
-    /* printf("matches %d\n", matches[j]); */
+    /* printf("matches %d\n", matches[*j]); */
 
-    for (int k = j+1; k < length || previous_matches[k]; k++) {
+    for (int k = *j+1; k < len_t || previous_matches[k]; k++) {
         if (matches[k]) {
             found++;
             break;
@@ -105,26 +109,32 @@ int calculate_penalty(int j, int *matches, int *previous_matches, int length)
         skip++;
     }
 
-    j += skip;
+    *j += skip;
 
     if (!found)
-        penalty += skip * length * 2;
-    else
-        penalty += skip - (found * length);
+        return penalty + (skip * len_t * len_t);
 
-    return penalty;
+    int diff = (len_s + skip);
+    // Use power function to exagerate differences in skip distances
+    diff = diff * diff;
+    /* int new = penalty + (diff - (len_t / found)); */
+    int new = penalty + (diff - (found * len_t));
+    /* printf("j=%d\told: %d, new: %d\n", *j, penalty, new); */
+
+    return new;
 }
 
-int edit_distance(const Entry* str_s, const Entry* str_t)
+int edit_distance(const char* str_s, const Entry* str_t)
 {
     /* if (strcmp(str_s->string, str_t->string) == 0) */
     /*     return 0; */
 
-    if (str_s->length == 0)
+    int len_s = strlen(str_s);
+    if (len_s == 0)
         return str_t->length;
 
     if (str_t->length == 0)
-        return str_s->length;
+        return len_s;
 
     int line_above[str_t->length + 1];
     int current_line[str_t->length + 1];
@@ -138,26 +148,26 @@ int edit_distance(const Entry* str_s, const Entry* str_t)
     int penalty = 0;
 
     for (i = 0; i < str_t->length + 1; i++) {
+        matches[i] = 0;
         line_above[i] = i;
         *(previous_matches + i) = 0;
         /* print_i(i, str_t->string[i]); */
     }
     /* printf("\n   "); */
 
-    for (i = 0; i < str_t->length + 1; i++) {
-        /* print_j(i, line_above[i]); */
-        matches[i] = 0;
-        /* if (i == 10) */
-        /*     printf(" "); */
-    }
+    /* for (i = 0; i < str_t->length + 1; i++) { */
+    /*     print_j(i, line_above[i]); */
+    /*     if (i == 10) */
+    /*         printf(" "); */
+    /* } */
 
     /* printf("\n"); */
-    for (i = 0; i < str_s->length; i++) {
-        /* printf("%c  %d  ", str_s->string[i], i + 1); */
+    for (i = 0; i < len_s; i++) {
+        /* printf("%c  %d  ", str_s[i], i + 1); */
         current_line[0] = i + 1;
 
         for (j = 0; j < str_t->length; j++) {
-            chr_s = str_s->string[i];
+            chr_s = str_s[i];
             chr_t = str_t->string[j];
             cost = (strncmp(&chr_s, &chr_t, sizeof(char)) == 0) ? 0 : 1;
             /* printf("%d\t%c == %c, cost: %d, ==> ", j, chr_s, chr_t, cost); */
@@ -169,21 +179,19 @@ int edit_distance(const Entry* str_s, const Entry* str_t)
         }
         /* printf("\n"); */
 
-        for (j = 0; j < str_t->length + 1; j++) {
-            line_above[j] = current_line[j];
-
-            if (j == str_t->length)
-                continue;
-
+        for (j = 0; j < str_t->length + 1; j++)
             if (previous_matches[j])
-                penalty = calculate_penalty(j,
-                                            matches,
+                penalty = calculate_penalty(matches,
                                             previous_matches,
-                                            str_t->length);
-        }
+                                            len_s,
+                                            str_t->length,
+                                            penalty,
+                                            &j);
 
-        for (int j = 0; j < str_t->length; j++)
+        for (int j = 0; j < str_t->length; j++) {
+            line_above[j] = current_line[j];
             previous_matches[j] = matches[j];
+        }
 
         /* free(matches); */
         /* free(previous_matches); */
@@ -196,8 +204,20 @@ int entry_cmp(const void *a, const void *b)
 {
     const Entry *ia = *(const Entry **)a;
     const Entry *ib = *(const Entry **)b;
-    /* printf("%d - %d = %d\n", ia->rank, ib->rank, ia->rank - ib->rank); */
     return ia->rank - ib->rank;
+}
+
+void query(const int entry_count, const char *s, Entry **entries)
+{
+    char *str;
+
+    for (int i = 0; i < entry_count; i++) {
+        str = entries[i]->string;
+        int rank = edit_distance(s, entries[i]);
+        entries[i]->rank = rank;
+    }
+
+    qsort(entries, entry_count, sizeof(Entry *), entry_cmp);
 }
 
 int main()
@@ -205,38 +225,80 @@ int main()
     Entry** entries = malloc(2000 * sizeof(Entry *));
     int i;
     int entry_count = 0;
-    char *str;
-    /* int entry_count = 2; */
-    /* entries[0] = new_entry("doapp"); */
-    /* entries[1] = new_entry("that some of its noisiest aut"); */
-    /* Entry* i1 = new_entry("bob"); */
-    /* Entry* i2 = new_entry("bomy"); */
-    /* Entry* i1 = new_entry("Saturday"); */
-    /* Entry* i2 = new_entry("Sunday"); */
-    /* Entry* i1 = new_entry("kitten"); */
-    /* Entry* i2 = new_entry("sitting"); */
-    /* Entry* input = new_entry("obligdenoisergbp"); */
-    Entry* input = new_entry("lzc");
-    printf("input: %s\n", input->string);
+    char buf[MAXBUF] = {0};
+    int row_count, col_count;
+    int pos = 0;
+    int delete = FALSE;
 
     entry_from_stdin(entries, &entry_count);
 
-    /* printf("input: %s\n", entries[1]->string); */
-    /* printf("dist: %d\n", edit_distance(entries[0], entries[1])); */
-    for (i = 0; i < entry_count; i++) {
-        str = entries[i]->string;
-        int rank = edit_distance(input, entries[i]);
-        /* printf("%s - %d\n", str, rank); */
-        entries[i]->rank = rank;
+    freopen("/dev/tty", "rw", stdin);
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    getmaxyx(stdscr, row_count, col_count);
+    int limit = (entry_count < row_count - 2) ? entry_count : row_count - 2;
+
+    for (i = 0; i < limit; i++) {
+        move(i, 0);
+        wprintw(stdscr, entries[i]->string);
     }
 
-    qsort(entries, entry_count, sizeof(Entry *), entry_cmp);
+    move(row_count - 1, 0);
+    refresh();
 
-    /* printf("\n"); */
-    for (i = 0; i < entry_count; i++)
-        printf("%d\t%s - %d\n", i, entries[i]->string, entries[i]->rank);
+    while (TRUE) {
+        if ((buf[pos] = getch()) == ERR) {
+            move(row_count - 1, 0);
+            wprintw(stdscr, "ERROR");
+            getch();
+            break;
+        }
+
+        if (buf[pos] == '\n') {
+            buf[pos] = 0;
+            break;
+        }
+
+        else if (buf[pos] == 127) {
+            buf[pos--] = 0;
+            delete = TRUE;
+        }
+
+        else if (pos >= MAXBUF - 1)
+            continue;
+
+        else {
+            move(row_count - 1, pos);
+            waddch(stdscr, buf[pos++]);
+        }
+
+        erase();
+        query(entry_count, buf, entries);
+        limit = (entry_count < row_count - 2) ? entry_count : row_count - 2;
+
+        for (i = 0; i < limit; i++) {
+            move(i, 0);
+            wprintw(stdscr, entries[i]->string);
+        }
+
+        move(row_count - 1, 0);
+        wprintw(stdscr, buf);
+
+        if (delete) {
+            move(row_count - 1, pos);
+            delch();
+            delete = FALSE;
+        }
+
+        refresh();
+    }
+
+    keypad(stdscr, FALSE);
+    endwin();
+    printf("%s\n", buf);
 
     free_all(entries, entry_count);
-    del_entry(input);
     return 0;
 }
